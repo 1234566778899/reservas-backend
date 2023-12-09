2a4fddd337208eefa0bcb9b1e83b1e5b4105b5fc
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2023-12-09 11:06:46 -0500

    mongodb

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect('mongodb://localhost:27017/reservas', {
+mongoose.connect('mongodb+srv://61126863:CJ05yA55creGkCii@cluster0.iyqbg9l.mongodb.net/reservas', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(db => console.log('db connected'))
